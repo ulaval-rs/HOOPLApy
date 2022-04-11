d67690cb2983734259649900192aced8b7cfad72
olichose123 <oli.banville@gmail.com>
2022-04-11 12:19:59 -0400

    add black and flake8 as dev dependencies

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ from hoopla.pet_models.pet_model import load_pet_models
 from hoopla.hydro_models import load_hydrological_models
 from hoopla.calibration.calibration import make_calibration
 
-config = hoopla.load_config('./config.toml')
+config = hoopla.load_config("./config.toml")
 
 catchment_names = list_catchments(config.general.time_step)
 hydro_models = load_hydrological_models(config)
-pet_models = load_pet_models(config.general.time_step) if config.general.compute_pet else []
-sar_models = load_snow_models(config.general.time_step) if config.general.compute_snowmelt else []
+pet_models = (
+    load_pet_models(config.general.time_step) if config.general.compute_pet else []
+)
+sar_models = (
+    load_snow_models(config.general.time_step)
+    if config.general.compute_snowmelt
+    else []
+)
 
 
 # Remove this after exploration
 # ---------------------------------
-print('catchments', catchment_names)
-print('hydro_models', hydro_models)
-print('evapotranspiration_models', pet_models)
-print('snow_models', sar_models)
+print("catchments", catchment_names)
+print("hydro_models", hydro_models)
+print("evapotranspiration_models", pet_models)
+print("snow_models", sar_models)
 # ---------------------------------
 
 # You may want to select this models that you want here
