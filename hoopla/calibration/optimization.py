d08ef724aab6d0700cdf25666d8107a5fc382d39
Gabriel Couture <gacou54@gmail.com>
2022-06-30 08:46:22 -0400

    Minor refactoring

--- hoopla/calibration/optimization.py
+++ hoopla/calibration/optimization.py
@@ -17,19 +17,19 @@ def shuffled_complex_evolution(hydro_model: BaseHydroModel, ngs: int, max_iterat
         max_loop_inc=max_iteration
     )
 
-    results = spotpy.analyser.load_csv_results('sceua-data')
-    max_index = np.argmin(results['like1'])
-    best_param = results['par'][max_index], results['par_1'][max_index], results['par_2'][max_index], results['par_3'][max_index], results['par_4'][max_index], results['par_5'][max_index]
-    best_cost_function_value = results['like1'][max_index]
-
-    return best_param, best_cost_function_value
+    return _load_results('sceua-data')
 
 
 def dds(hydro_model: BaseHydroModel, max_iteration: int) -> tuple[Sequence[float], float]:
-    sampler = spotpy.algorithms.dds(hydro_model, dbname='dss-data', dbformat='csv')
+    sampler = spotpy.algorithms.dds(hydro_model, dbname='dds-data', dbformat='csv')
     sampler.sample(repetitions=max_iteration)
 
-    results = spotpy.analyser.load_csv_results('dds-data')
+    return _load_results('dds-data')
+
+
+def _load_results(filename: str) -> tuple[Sequence[float], float]:
+    results = spotpy.analyser.load_csv_results(filename)
+
     max_index = np.argmin(results['like1'])
     best_param = results['par'][max_index], results['par_1'][max_index], results['par_2'][max_index], results['par_3'][max_index], results['par_4'][max_index], results['par_5'][max_index]
     best_cost_function_value = results['like1'][max_index]
